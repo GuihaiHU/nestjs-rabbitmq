03e548f6a5532088fa89e3c26659d73879a8e947
huguihai <hu.guihai@qq.com>
2020-05-06 21:46:05 +0800

    fix: mq链接失败提醒

--- src/rabbitmq.provider.ts
+++ src/rabbitmq.provider.ts
@@ -10,7 +10,16 @@ export function rabbitmqConnectionFactory(connectionOptions: RabbitmqConnectionO
   return {
     provide: getRabbitmqConnectionToken(connectionOptions.name),
     useFactory: async (rabbitmqConnection: RabbitmqConnection): Promise<RabbitmqConnection> => {
+      let hasConnect = false
       const connection = connect(connectionOptions.urls, connectionOptions.options);
+      connection.on('connect', () => {
+        hasConnect = true
+      });
+      setTimeout(() => {
+        if (!hasConnect) {
+          throw new Error('Rabbitmq not connect in 5 seconds. Please Check')
+        }
+      }, 5000);
       rabbitmqConnection.setConnection(connection);
       RabbitmqContainer.set(connectionOptions.name, rabbitmqConnection);
       return rabbitmqConnection;
