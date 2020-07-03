ff54dea15db8ff601f53796a6e0d93b341667df9
huguihai <hu.guihai@qq.com>
2020-07-03 15:24:06 +0800

    test: 完善测试

--- test/rabbitmq.test.ts
+++ test/rabbitmq.test.ts
@@ -1,22 +1,117 @@
-import { Injectable, Module, Global } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RabbitmqModule, RabbitmqSubscribe } from '../src';
 import { RabbitmqConnection } from '../src/rabbitmq.connection';
 
 describe('测试rabbitmq启动', () => {
   @Injectable()
-  class RabbitMqService {
+  class SingleQueueRabbitMqService {
+    info = { test: 'hello' };
+    result = { isReceive: false, data: null };
     constructor(private readonly amqpConnection: RabbitmqConnection) {}
 
     async sendInfo() {
-      await this.amqpConnection.publish('test', 'routingKey', {
-        msg: 'Hello World!',
+      await this.amqpConnection.publish('test', 'routingKey', this.info);
+      return new Promise((resolve, reject) => {
+        let _i = setInterval(() => {
+          if (this.result.isReceive) {
+            expect(this.result.data).toEqual(this.info);
+            resolve();
+          }
+        }, 100);
+        setTimeout(() => {
+          if (!this.result.isReceive) {
+            clearInterval(_i);
+            reject('消费者没有收到消息');
+          }
+        }, 2000);
       });
     }
 
     @RabbitmqSubscribe('test', 'routingKey', 'queue')
-    async getInfo(info) {
-      console.log(info);
+    async getInfo(info: any) {
+      this.result.data = info;
+      this.result.isReceive = true;
+    }
+  }
+
+  @Injectable()
+  class MultiQueueRabbitMqService {
+    info = { test: 'hello' };
+    result = { isReceive1: false, data1: null, isReceive2: false, data2: null };
+    constructor(private readonly amqpConnection: RabbitmqConnection) {}
+
+    async sendInfo() {
+      await this.amqpConnection.publish('test', 'routingKey', this.info);
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (!this.result.isReceive1 && !this.result.isReceive2) {
+            reject('消费者没有收到消息');
+          } else {
+            expect(Object.assign({}, this.result.data1, this.result.data2)).not.toEqual({});
+            if (!this.result.data1 && this.result.data2) {
+              expect(this.result.data2).toEqual(this.info);
+            } else if (!this.result.data2 && this.result.data1) {
+              expect(this.result.data1).toEqual(this.info);
+            } else {
+              reject('消费者没有收到消息');
+            }
+            resolve();
+          }
+        }, 2000);
+      });
+    }
+
+    @RabbitmqSubscribe('test', 'routingKey', 'queue')
+    async getInfo1(info: any) {
+      this.result.data1 = info;
+      this.result.isReceive1 = true;
+    }
+
+    @RabbitmqSubscribe('test', 'routingKey', 'queue')
+    async getInfo2(info: any) {
+      this.result.data2 = info;
+      this.result.isReceive2 = true;
+    }
+  }
+
+  @Injectable()
+  class UnnamedQueueRabbitMqService {
+    info = { test: 'hello' };
+    result = { isReceive1: false, data1: null, isReceive2: false, data2: null };
+    constructor(private readonly amqpConnection: RabbitmqConnection) {}
+
+    async sendInfo() {
+      await this.amqpConnection.publish('test', 'UnnamedQueueRabbitMqService', this.info);
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (!this.result.isReceive1 && !this.result.isReceive2) {
+            reject('消费者没有收到消息');
+          } else {
+            expect(Object.assign({}, this.result.data1, this.result.data2)).not.toEqual({});
+            if (!this.result.data1 && this.result.data2) {
+              expect(this.result.data2).toEqual(this.info);
+            } else if (!this.result.data2 && this.result.data1) {
+              expect(this.result.data1).toEqual(this.info);
+            } else {
+              reject('消费者没有收到消息');
+            }
+            resolve();
+          }
+        }, 2000);
+      });
+    }
+
+    @RabbitmqSubscribe('test', 'UnnamedQueueRabbitMqService')
+    async getInfo1(info: any) {
+      this.result.data1 = info;
+      this.result.isReceive1 = true;
+    }
+
+    @RabbitmqSubscribe('test', 'UnnamedQueueRabbitMqService')
+    async getInfo2(info: any) {
+      this.result.data2 = info;
+      this.result.isReceive2 = true;
     }
   }
 
@@ -27,7 +122,7 @@ describe('测试rabbitmq启动', () => {
           urls: ['amqp://server:123456@localhost:5672?heartbeat=60'],
         }),
       ],
-      providers: [RabbitMqService],
+      providers: [SingleQueueRabbitMqService],
     }).compile();
 
     const app = module.createNestApplication();
@@ -44,7 +139,7 @@ describe('测试rabbitmq启动', () => {
             urls: ['amqp://server1:errorPassword@localhost:5672?heartbeat=60'],
           }),
         ],
-        providers: [RabbitMqService],
+        providers: [SingleQueueRabbitMqService],
       }).compile();
       let app = module.createNestApplication();
       await app.init();
@@ -79,7 +174,7 @@ describe('测试rabbitmq启动', () => {
           inject: [ConfigService],
         }),
       ],
-      providers: [RabbitMqService],
+      providers: [SingleQueueRabbitMqService],
     }).compile();
 
     const app = module.createNestApplication();
@@ -113,7 +208,7 @@ describe('测试rabbitmq启动', () => {
             inject: [ConfigService],
           }),
         ],
-        providers: [RabbitMqService],
+        providers: [SingleQueueRabbitMqService],
       }).compile();
 
       const app = module.createNestApplication();
@@ -124,4 +219,58 @@ describe('测试rabbitmq启动', () => {
       done();
     }
   }, 10000);
+
+  it('消息获取-具名队列-单消费者', async done => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        RabbitmqModule.register({
+          urls: ['amqp://server:123456@localhost:5672?heartbeat=60'],
+        }),
+      ],
+      providers: [SingleQueueRabbitMqService],
+    }).compile();
+
+    const app = module.createNestApplication();
+    await app.init();
+    const rabbitmqService = module.get<SingleQueueRabbitMqService>(SingleQueueRabbitMqService);
+    await rabbitmqService.sendInfo();
+    await app.close();
+    done();
+  });
+
+  it('消息获取-具名队列-多消费者', async done => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        RabbitmqModule.register({
+          urls: ['amqp://server:123456@localhost:5672?heartbeat=60'],
+        }),
+      ],
+      providers: [MultiQueueRabbitMqService],
+    }).compile();
+
+    const app = module.createNestApplication();
+    await app.init();
+    const rabbitmqService = module.get<MultiQueueRabbitMqService>(MultiQueueRabbitMqService);
+    await rabbitmqService.sendInfo();
+    await app.close();
+    done();
+  });
+
+  it('消息获取-匿名队列', async done => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        RabbitmqModule.register({
+          urls: ['amqp://server:123456@localhost:5672?heartbeat=60'],
+        }),
+      ],
+      providers: [UnnamedQueueRabbitMqService],
+    }).compile();
+
+    const app = module.createNestApplication();
+    await app.init();
+    const rabbitmqService = module.get<UnnamedQueueRabbitMqService>(UnnamedQueueRabbitMqService);
+    await rabbitmqService.sendInfo();
+    await app.close();
+    done();
+  });
 });
